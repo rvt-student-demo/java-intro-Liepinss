1b21c2ffd42ff3ca3c092f827b55c4d9f8fea63f
Liepinss <166013478+Liepinss@users.noreply.github.com>
2024-09-25 06:03:46 +0000

    grades and paoints

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,26 +1,37 @@
 package lv.rvt;
 
-public class App 
+public  class App
 {
-    public static void main( String[] args )
-    {
-    scanner scanner = new scanner(system.in);
-
-    systemm.out.println("ievadi savu vārdu: ");
-    systemm.out.println("Sveiks student: ");
-    string name = scanner.next.line();
-
-    system.out.println("Ievediet savu uzvārdu");
-    starting group = scanner.nextLine();
-    
-    system.out.println( "ievadi grupu");
-    string grupa = scanner.nextLine();
-
-
-    system.out.println();
-
-
+    public static void main(String[] args) {
+        
     }
+Scanner scanner = new Scanner(System.in);
 
+System.out.println("Give points [0-100]:");
+int points = Integer.valueOf(scanner.nextLine());
+if ( points < 0) {
+    System.out.println("impossible!");
+}
+else if (points <= 49){
+    System.out.println("grade: failed");
+}
+else if (points <= 59){
+    System.out.println("grade: 1");
+}
+else if (points <= 69){
+    System.out.println("grade: 2");
+}
+else if (points <= 79){
+    System.out.println("grade: 3");
+}
+else if (point <= 89){
+    System.out.println("grade: 4");
+}
+else if (point <= 100){
+    System.out.println("grade: 5");
+}
+else {
+    System.out.println("grade: incredible!");
+}
 
 }
