9edff828e061a06b5c6693a9c46a15aa8e9780be
Liepinss <166013478+Liepinss@users.noreply.github.com>
2024-10-02 06:53:38 +0000

    part 4

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,32 +2,44 @@ package lv.rvt;
 import java.util.Scanner;
 public class App
 {
+    
 
-
-    public class SumAndCount {
+    public class AverageCalculator {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Give numbers:");
-    
             int sum = 0;
             int count = 0;
+            int number;
     
+            System.out.println("Give numbers:");
+    
+            
             while (true) {
-                int number = scanner.nextInt();
+                number = scanner.nextInt();
+    
                 if (number == -1) {
-                    System.out.println("Thx! Bye!");
-                    break;
+                    break; 
                 }
-                sum += number;  
-                count++;        
+    
+                sum += number; 
+                count++;       
             }
     
+            scanner.close(); 
+    
+            
+            System.out.println("Thx! Bye!");
             System.out.println("Sum: " + sum);
             System.out.println("Numbers: " + count);
-            scanner.close();
+    
+            if (count > 0) {
+                double average = (double) sum / count; 
+                System.out.println("Average: " + average);
+            } else {
+                System.out.println("Average: N/A"); 
+            }
         }
     }
-    
     
 }
 
