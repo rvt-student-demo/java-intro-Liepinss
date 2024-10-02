405e1c96b49ac171998b7ec1f94d3d4d15e072ae
Liepinss <166013478+Liepinss@users.noreply.github.com>
2024-10-02 06:49:24 +0000

    part 3

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,14 +2,15 @@ package lv.rvt;
 import java.util.Scanner;
 public class App
 {
-    
 
-    public class SumOfNumbers {
+
+    public class SumAndCount {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Give numbers:");
     
             int sum = 0;
+            int count = 0;
     
             while (true) {
                 int number = scanner.nextInt();
@@ -17,13 +18,16 @@ public class App
                     System.out.println("Thx! Bye!");
                     break;
                 }
-                sum += number; 
+                sum += number;  
+                count++;        
             }
     
             System.out.println("Sum: " + sum);
+            System.out.println("Numbers: " + count);
             scanner.close();
         }
     }
+    
     
 }
 
