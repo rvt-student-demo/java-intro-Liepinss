cc7d68620d2f1b397e3aa06430b18bffaf764f5d
Liepinss <166013478+Liepinss@users.noreply.github.com>
2024-10-02 05:45:13 +0000

    sun of a sequnce

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,18 +2,26 @@ package lv.rvt;
 import java.util.Scanner;
 public class App
 {
-public class Counting {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        int number = scanner.nextInt();
-
-        for (int i = 0; i <= number; i++) {
-            System.out.println(i);
+    public class SumOfSequence {
+        public static void main(String[] args) {
+            
+            Scanner scanner = new Scanner(System.in);
+    
+            System.out.print("Last number? ");
+            int number = scanner.nextInt();
+    
+            int sum = 0;
+    
+            for (int i = 1; i <= number; i++) {
+                sum += i;
+            }
+    
+            System.out.println("The sum is " + sum);
+    
+            scanner.close();
         }
-        scanner.close();
     }
-}
 
 }
 
