7acd60ff69dad9fa798366c409529ace3679557c
Liepinss <166013478+Liepinss@users.noreply.github.com>
2024-10-09 07:05:53 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,28 +3,26 @@ import java.util.Scanner;
 public class App
 {
     public class Main {
-
         public static void divisibleByThreeInRange(int beginning, int end) {
-            
-            for (int i = beginning; i <= end; i++) {
-               
-                if (i % 3 == 0) {
-                    
-                    System.out.println(i);
-                }
-            }
+        for (int i = beginning; i <= end; i++) {
+        if (i % 3 == 0) {
+        System.out.println(i);
         }
-    
+        }
+        }
+        
         public static void main(String[] args) {
-           
-            divisibleByThreeInRange(3, 6);
-            System.out.println(); 
-    
-           
-            divisibleByThreeInRange(2, 10);
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ievadiet diapazona sākumu: ");
+        int beginning = scanner.nextInt();
+        
+        System.out.print("Ievadiet diapazona beigas: ");
+        int end = scanner.nextInt();
+        
+        divisibleByThreeInRange(beginning, end);
         }
-    }
-    
+        }
 }
 
     
