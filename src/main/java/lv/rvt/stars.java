89150e6d92286d566157890b4a332a6838cba0e6
Liepinss <166013478+Liepinss@users.noreply.github.com>
2024-10-23 07:18:33 +0000

    Only these numbers

--- src/main/java/lv/rvt/stars.java
+++ src/main/java/lv/rvt/stars.java
@@ -23,4 +23,35 @@ public static void printStars(int number){
             printStars(i);
         }
     }
+    public class ChristmasTree {
+        public static void christmasTree(int n) {
+            
+            for (int i = 1; i <= n; i++) {
+               
+                for (int j = i; j < n; j++) {
+                    System.out.print(" ");
+                }
+                
+                for (int j = 1; j <= (2 * i - 1); j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            
+            
+            for (int i = 0; i < 2; i++) {
+                
+                for (int j = 1; j < n - 1; j++) {
+                    System.out.print(" ");
+                }
+                
+                System.out.println("***");
+            }
+        }
+    
+        public static void main(String[] args) {
+            christmasTree(10);
+        }
+    }
+    
 }
