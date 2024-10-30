87dda35691fe9c2ccd6f9a768b9bcf4ba154c7c9
Liepinss <166013478+Liepinss@users.noreply.github.com>
2024-10-30 08:50:08 +0000

    array

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,28 +1,38 @@
 package lv.rvt;
 import java.util.Scanner;
 import java.util.ArrayList;
-public class App
 
-{
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
-        while (true) {
-            System.out.print("Enter a number: ");
-            int number = scanner.nextInt();
-            if (number == -1) {
-                break;
-            }
-            numbers.add(number);
-        }
-        System.out.print("From where? ");
-        int startIndex = scanner.nextInt();
-        System.out.print("To where? ");
-        int endIndex = scanner.nextInt();
-        for (int i = startIndex; i <= endIndex; i++) {
-            System.out.println(numbers.get(i));
-        }
-    }
+
+
+ import java.util.Random;
+
+public class App {
+public static void main(String[] args) {
+Random random = new Random();
+int[] array = new int[10];
+
+for (int i = 0; i < array.length; i++) {
+array[i] = random.nextInt(100);
 }
+
+System.out.println("Array elements:");
+for (int num : array) {
+System.out.print(num + " ");
+}
+System.out.println();
+
+int max = array[0];
+int min = array[0];
+for (int num : array) {
+if (num > max) max = num;
+if (num < min) min = num;
+}
+
+System.out.println("Maximum value: " + max);
+System.out.println("Minimum value: " + min);
+}
+}
+    
+
 
     
