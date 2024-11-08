0d86eaa309abfc70aec23c243489a4c2de5f3c58
Liepinss <166013478+Liepinss@users.noreply.github.com>
2024-11-08 09:03:38 +0000

    product

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,37 +2,14 @@ package lv.rvt;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
-public class App
 
-{
+
+public class App{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> names = new ArrayList<>();
-        ArrayList<Integer> birthYears = new ArrayList<>();
-        while (true) {
-        String input = scanner.nextLine();
-        if (input.isEmpty()) {
-        break;
-        }
         
-        String[] parts = input.split(",");
-        names.add(parts[0]);
-        birthYears.add(Integer.parseInt(parts[1]));
-        }
-        String longestName = "";
-        for (String name : names) {
-        if (name.length() > longestName.length()) {
-        longestName = name;
-        }
-        }
-        int sum = 0;
-        for (int year : birthYears) {
-        sum += year;
-        }
-        double average = sum / (double) birthYears.size();
-        System.out.println("Longest name: " + longestName);
-        System.out.println("Average of the birth years: " + average);
-        }
+        Product banana = new Product("Banana", 1.1, 13);
+        banana.printProduct();
+    }
 }
     
 
