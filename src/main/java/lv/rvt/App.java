b9fdc5c5c664cda5164e7d4d37c56d013532d499
Liepinss <166013478+Liepinss@users.noreply.github.com>
2025-01-16 07:36:21 +0000

    box chapter 50

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class App
-{ public static void main(String[] args) {
-person paul = new person("Paul", 24);
-person ada = new person("Ada");
-
-System.out.println(paul);
-System.out.println(ada);
+{
+public static void main(String[] args) {
+Box box = new Box(10, 5, 2);
+System.out.println(box);
+Box bigger = box.biggerBox();
+Box smaller = box.smallerBox();
+System.out.println("Bigger Box: " + bigger);
+System.out.println("Smaller Box: " + smaller);
+Box largeBox = new Box(20, 10, 5);
+System.out.println("Does box nest in largeBox? " + box.nests(largeBox));
+System.out.println("Does largeBox nest in box? " + largeBox.nests(box));
 }
 }
